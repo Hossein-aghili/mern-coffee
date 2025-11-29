835fb3d2c9670d1df9c67184c8102f5a212b8866
Hossein <hossein.aghili.2658@gmail.com>
2025-11-29 10:41:09 +0330

    import morgan

--- api/app.js
+++ api/app.js
@@ -2,9 +2,11 @@ import { fileURLToPath } from "url";
 import path from "path";
 import express from "express"
 import cors from 'cors'
+import morgan from "morgan";
 const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(morgan("dev"))
