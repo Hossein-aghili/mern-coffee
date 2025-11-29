0a1998649adf9bc99ffdf5d48f8c293e34004931
Hossein <hossein.aghili.2658@gmail.com>
2025-11-29 10:57:29 +0330

    port running

--- api/servre.js
+++ api/servre.js
@@ -8,3 +8,7 @@ dotenv.configDotenv({ path: __dirname + '/config.env' })
 mongoose.connect(process.env.DATA_BASE).then(() => {
     console.log('database is connect')
 }).catch(err => console.log(err))
+
+app.listen(process.env.PORT, () => {
+    console.log('server is running')
+})
