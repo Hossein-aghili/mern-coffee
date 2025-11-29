640bbbe263529e14cfe545c2d61f18fccca0bc64
Hossein <hossein.aghili.2658@gmail.com>
2025-11-29 10:52:22 +0330

    import dotenv file

---
+++ api/servre.js
@@ -0,0 +1,7 @@
+import app from "./app.js";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+import { __dirname } from "./app.js";
+
+dotenv.configDotenv({path:__dirname +'/config.env'})
+
