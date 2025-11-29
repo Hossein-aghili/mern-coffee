7252f5e2215b461a18024d7cdfae6e027772f084
Hossein <hossein.aghili.2658@gmail.com>
2025-11-29 10:40:17 +0330

    import cors

--- api/app.js
+++ api/app.js
@@ -1,5 +1,10 @@
 import { fileURLToPath } from "url";
 import path from "path";
-
+import express from "express"
+import cors from 'cors'
 const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
+
+const app = express()
+app.use(cors())
+app.use(express.json())
