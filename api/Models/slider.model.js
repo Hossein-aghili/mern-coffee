3c8c2da96634958ed608aa4a3a2da3b8b1c7e831
Hossein <hossein.aghili.2658@gmail.com>
2025-11-30 07:48:40 +0330

    create slider model Schema

---
+++ api/Models/slider.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const sliderSchema = mongoose.Schema({
+    name: {
+        type: String
+    },
+    images: {
+        type: [String],
+        required: [true, 'image is required']
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+}, { timestamps: true })
+
+const Slider = mongoose.model('Slider', sliderSchema)
+export default Slider
