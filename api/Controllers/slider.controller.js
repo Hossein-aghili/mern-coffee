42eb99585acb5768891f164e3c530f770b9359f5
Hossein <hossein.aghili.2658@gmail.com>
2025-11-30 08:26:34 +0330

    create function update slider

--- api/Controllers/slider.controller.js
+++ api/Controllers/slider.controller.js
@@ -29,11 +29,28 @@ export const getOne = catchAsync(async (req, res, next) => {
     const { id } = req.params
     const slider = await Slider.findById(id)
     if (!slider) {
-        return next(new HandleERROR('اسلایدر پیدا نشد'), 404)
+        return next(new HandleERROR('اسلایدر پیدا نشد',404))
     }
     return res.status(201).json({
         success: true,
         data: slider,
         message: 'اسلایدر با موفقیت دریافت شد'
+    })
+})
+export const update = catchAsync(async (req, res, next) => {
+    const { id } = req.params
+    let slider = await Slider.findByIdAndUpdate(id, req.body, {
+        new: true,
+        runValidators: true
+    })
+    if (!slider) {
+        return next(new HandleERROR('اسلایدر پیدا نشد', 404))
+    }
+    slider.isActive = !slider.isActive
+    await slider.save()
+    return res.status(200).json({
+        success:true,
+        data:slider,
+        message:'اسلایدر با موفقیت آپدیت شد'
     })
 })
