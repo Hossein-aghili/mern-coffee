42b4565c78b7ffe65d8e65395328faf689b39604
Hossein <hossein.aghili.2658@gmail.com>
2025-11-30 08:37:52 +0330

    Add slider route to app.js

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ import path from "path";
 import express from "express"
 import cors from 'cors'
 import morgan from "morgan";
+import sliderRouter from "./Routes/slider.route.js";
 const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
 
@@ -11,6 +12,7 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(express.static("Public"))
+app.use('/api/slider',sliderRouter)
 
 
 export default app
