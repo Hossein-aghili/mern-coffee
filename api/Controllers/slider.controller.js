08a820dc6b835efa408315a025f9f557a7483ecd
Hossein <hossein.aghili.2658@gmail.com>
2025-11-30 07:56:25 +0330

    create function create slider

--- api/Controllers/slider.controller.js
+++ api/Controllers/slider.controller.js
@@ -1,3 +1,12 @@
 import Slider from "../Models/slider.model.js";
 import { HandleERROR } from "vanta-api";
 import catchAsync from "vanta-api";
+
+export const create = catchAsync(async (req, res, next) => {
+    const slider = await Slider.create(req.body)
+    return res.status(200).json({
+        success: true,
+        data: slider,
+        message: 'اسلایدر با موفقیت ساخته شد'
+    })
+})
