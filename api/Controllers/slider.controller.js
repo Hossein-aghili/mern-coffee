d552c0207bd7cdbcf34c7038ce1d96c564f059fe
Hossein <hossein.aghili.2658@gmail.com>
2025-11-30 08:06:01 +0330

    create function getAll slider

--- api/Controllers/slider.controller.js
+++ api/Controllers/slider.controller.js
@@ -1,5 +1,5 @@
 import Slider from "../Models/slider.model.js";
-import { HandleERROR } from "vanta-api";
+import ApiFeatures, { HandleERROR } from "vanta-api";
 import catchAsync from "vanta-api";
 
 export const create = catchAsync(async (req, res, next) => {
@@ -8,5 +8,20 @@ export const create = catchAsync(async (req, res, next) => {
         success: true,
         data: slider,
         message: 'اسلایدر با موفقیت ساخته شد'
+    })
+})
+export const getAll = catchAsync(async (req, res, next) => {
+    const features = ApiFeatures(Slider, req.query, req.role)
+        .filter()
+        .sort()
+        .populate()
+        .paginate()
+        .limitFields()
+    const sliders = await features.execute()
+    const count = await Slider.countDocuments()
+    return res.status(200).json({
+        success: true,
+        data: sliders,
+        count
     })
 })
