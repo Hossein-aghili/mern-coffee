4499384b1b8519a96c522de7d8ab8b0fbf2efa00
Hossein <hossein.aghili.2658@gmail.com>
2025-12-01 06:45:04 +0330

    create function getAll blog

--- api/Controllers/blog.controller.js
+++ api/Controllers/blog.controller.js
@@ -1,11 +1,26 @@
 import Blog from "../Models/blog.model.js";
-import { catchAsync, HandleERROR } from "vanta-api";
+import ApiFeatures, { catchAsync, HandleERROR } from "vanta-api";
 
 export const create = catchAsync(async (req, res, next) => {
     const blog = await Blog.create(req.body)
     return res.status(200).json({
+        success: true,
+        data: blog,
+        message: 'وبلاگ با موفقیت ساخته شد'
+    })
+})
+export const getAll = catchAsync(async (req, res, next) => {
+    const features = new ApiFeatures(Blog, req.query, req.role)
+        .filter()
+        .sort()
+        .limitFields()
+        .populate()
+        .paginate()
+    const blogs = await features.execute()
+    const count = await Blog.countDocuments()
+    return res.status(200).json({
         success:true,
-        data:blog,
-        message:'وبلاگ با موفقیت ساخته شد'
+        data:blogs,
+        count
     })
 })
