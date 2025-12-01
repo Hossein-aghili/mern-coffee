cb73aedca90591176f16f6a95074e4f34482413e
Hossein <hossein.aghili.2658@gmail.com>
2025-12-01 09:30:11 +0330

    create function getAll product

--- api/Controllers/product.controller.js
+++ api/Controllers/product.controller.js
@@ -1,5 +1,5 @@
 import Product from "../Models/product.model.js";
-import { catchAsync, HandleERROR } from "vanta-api";
+import ApiFeatures, { catchAsync, HandleERROR } from "vanta-api";
 
 export const create = catchAsync(async (req, res, next) => {
     const product = await Product.create(req.body)
@@ -7,5 +7,20 @@ export const create = catchAsync(async (req, res, next) => {
         success: true,
         data: product,
         message: 'محصول با موفقیت ساخته شد'
+    })
+})
+export const getAll = catchAsync(async (req, res, next) => {
+    const features = new ApiFeatures(Product, req.query, req.role)
+        .filter()
+        .sort()
+        .populate()
+        .paginate()
+        .limitFields()
+    const products = await features.execute()
+    const count = await Product.countDocuments()
+    return res.status(200).json({
+        success: true,
+        data: products,
+        count
     })
 })
