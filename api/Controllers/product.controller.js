9b606179106218d49bfb5dc71ec2a3a8a0668ef5
Hossein <hossein.aghili.2658@gmail.com>
2025-12-01 09:34:06 +0330

    create function getOne product

--- api/Controllers/product.controller.js
+++ api/Controllers/product.controller.js
@@ -24,3 +24,15 @@ export const getAll = catchAsync(async (req, res, next) => {
         count
     })
 })
+export const getOne = catchAsync(async (req, res, next) => {
+    const { id } = req.params
+    const prosuct = await Product.findById(id).populate('CategoryId')
+    if (!prosuct) {
+        return next(new HandleERROR('محصول پیدا نشد', 404))
+    }
+    return res.status(200).json({
+        success: true,
+        data: prosuct,
+        message: 'محصول با موفقیت دریافت شد'
+    })
+})
