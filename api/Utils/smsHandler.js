a1e8ad156cb396315faa567335e1c98b626ea076
Hossein <hossein.aghili.2658@gmail.com>
2025-12-02 08:27:50 +0330

    create function verifyCode utils

--- api/Utils/smsHandler.js
+++ api/Utils/smsHandler.js
@@ -17,3 +17,22 @@ export const sendAuthCode=async(Mobile)=>{
         return {success:false,message:'خطا در ارسال پیامک'}
     }
 }
+export const verifyCode=async(Mobile,Code)=>{
+  try {
+      const res=await fetch('https://api.limosms.com/api/checkcode',{
+        method:'POST',
+        headers:{
+          'Content-type':'application/json',
+          ApiKey:process.env.SMS_KEY
+        },
+        body:JSON.stringify({
+          Mobile,
+          Code
+        })
+      })
+      const data=await res.json()
+      return data
+  } catch (error) {
+      return {success:false,message:'خطا در بررسی کد'}
+  }
+}
