2e37b55e2e1286afdf7ab7ec295bfa88fbb06bf9
Hossein <hossein.aghili.2658@gmail.com>
2025-12-03 07:36:04 +0330

    Add auth and user route to app.js

--- api/app.js
+++ api/app.js
@@ -1,24 +1,27 @@
 import { fileURLToPath } from "url";
 import path from "path";
-import express from "express"
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 import morgan from "morgan";
 import sliderRouter from "./Routes/slider.route.js";
 import categoryRouter from "./Routes/category.route.js";
 import blogRouter from "./Routes/blog.route.js";
 import productRouter from "./Routes/product.route.js";
+import authRouter from "./Routes/auth.route.js";
+import userRouter from "./Routes/user.route.js";
 const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(morgan("dev"))
-app.use(express.static("Public"))
-app.use('/api/slider',sliderRouter)
-app.use('/api/category',categoryRouter)
-app.use('/api/blog',blogRouter)
-app.use('/api/product',productRouter)
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static("Public"));
+app.use("/api/auth", authRouter);
+app.use("/api/slider", sliderRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/blog", blogRouter);
+app.use("/api/product", productRouter);
+app.use("/api/user", userRouter);
 
-
-export default app
+export default app;
