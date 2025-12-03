1b24bf5a2c108a8baa717ec67af84b571991a211
Hossein <hossein.aghili.2658@gmail.com>
2025-12-03 07:18:52 +0330

    create function update controller user

--- api/Controllers/user.controller.js
+++ api/Controllers/user.controller.js
@@ -32,3 +32,25 @@ export const getOne = catchAsync(async (req, res, next) => {
     message: "با موفقیت ساخته شد",
   });
 });
+export const update = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  if (req.role !== "admin" && req.userId !== id) {
+    return next(new HandleERROR("شما اجازه دسترسی به این صفحه را ندارید", 403));
+  }
+  const { fullName = null, username = null, role = null } = req.body;
+  const user = await User.findById(id);
+  if (!user) {
+    return next(new HandleERROR("کاربر پیدا نشد", 400));
+  }
+  user.fullName = fullName || user.fullName;
+  user.username = username || user.username;
+  if (req.role == "admin" && role) {
+    user.role = role;
+  }
+  const newUser = await user.save()
+      return res.status(200).json({
+        success: true,
+        data: newUser,
+        message: "با موفقیت آپدیت شد",
+      });
+});
